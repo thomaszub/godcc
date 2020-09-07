73215aeb04df23d50a5049ff3449bee4af238789
Thomas Zub <thomas.zub@codecentric.de>
2020-09-07 17:23:57 +0200

    Restructured dcc_test to simplify appending of new tests

--- dcc_test.go
+++ dcc_test.go
@@ -5,47 +5,71 @@ import (
 	"time"
 )
 
+type DcTest struct {
+	from   Date
+	to     Date
+	dcExps []DcExp
+}
+
+type DcExp struct {
+	dcc DayCounter
+	exp float64
+}
+
 func date(year int, month time.Month, day int) Date {
 	return time.Date(year, month, day, 12, 0, 0, 0, time.UTC)
 }
 
 func TestDayCountFractions(t *testing.T) {
-	tests := []struct {
-		from Date
-		to   Date
-		exp  float64
-	}{
+	tests := []DcTest{
 		{
 			from: date(2018, 3, 31),
 			to:   date(2018, 3, 31),
-			exp:  0.0,
+			dcExps: []DcExp{
+				{
+					dcc: D30360{},
+					exp: 0,
+				},
+			},
 		},
 		{
 			from: date(2018, 4, 30),
 			to:   date(2018, 7, 31),
-			exp:  0.25,
+			dcExps: []DcExp{
+				{
+					dcc: D30360{},
+					exp: 0.25,
+				},
+			},
 		},
 		{
 			from: date(2018, 3, 31),
 			to:   date(2018, 9, 30),
-			exp:  0.5,
+			dcExps: []DcExp{
+				{
+					dcc: D30360{},
+					exp: 0.5,
+				},
+			},
 		},
 		{
 			from: date(2018, 3, 31),
 			to:   date(2019, 3, 31),
-			exp:  1.0,
+			dcExps: []DcExp{
+				{
+					dcc: D30360{},
+					exp: 1.0,
+				},
+			},
 		},
 	}
 
-	dayCounters := []DayCounter{
-		D30360{},
-	}
-
-	for _, dc := range dayCounters {
-		for _, tt := range tests {
-			res := dc.Frac(tt.from, tt.to)
-			if res != tt.exp {
-				t.Errorf("Wrong result for DayCounter %T from %s to %s. got=%f, exp=%f", dc, tt.from, tt.to, res, tt.exp)
+	for _, tt := range tests {
+		for _, dct := range tt.dcExps {
+			res := dct.dcc.Frac(tt.from, tt.to)
+			t.Logf("Testing DayCounter %T from %s to %s", dct.dcc, tt.from, tt.to)
+			if res != dct.exp {
+				t.Errorf("Wrong result for DayCounter %T from %s to %s. got=%f, exp=%f", dct.dcc, tt.from, tt.to, res, dct.exp)
 			}
 		}
 	}
