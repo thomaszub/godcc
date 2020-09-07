8d4e6480dda39f96a5cf4a1ebf735e5ee48000d8
Thomas Zub <thomas.zub@codecentric.de>
2020-09-07 17:31:35 +0200

    Refactoring

--- d30360.go
+++ d30360.go
@@ -1,5 +1,9 @@
 package godcc
 
+func base30360(y1, y2, m1, m2, d1, d2 int) float64 {
+	return float64(360*(y2-y1)+30*(m2-m1)+d2-d1) / 360.0
+}
+
 type D30360 struct{}
 
 func (_ D30360) Frac(from, to Date) float64 {
@@ -9,8 +13,4 @@ func (_ D30360) Frac(from, to Date) float64 {
 		d2 = min(d2, 30)
 	}
 	return base30360(from.Year(), to.Year(), int(from.Month()), int(to.Month()), d1, d2)
-}
-
-func base30360(y1, y2, m1, m2, d1, d2 int) float64 {
-	return float64(360*(y2-y1)+30*(m2-m1)+d2-d1) / 360.0
 }
